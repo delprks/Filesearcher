9397f4b6b0f1e052f5da2c4232b8aa946dc8a98e
Daniel Parks <daniel.parks@bbc.co.uk>
2015-12-29 13:24:13 +0000

    refactoring

--- src/test/scala/fileSearcher/FilterCheckerTests.scala
+++ src/test/scala/fileSearcher/FilterCheckerTests.scala
@@ -6,15 +6,14 @@ import java.io.File
 class FilterCheckerTests extends FlatSpec{
   "FilterChecker passed a list where one file matches the filter" should
   "return a list with that file" in {
-    val matchingFile = new FileObject(new File("match"))
-    val listOfFiles = List(new FileObject(new File("random")), matchingFile)
+    val listOfFiles = List(FileObject(new File("random")), FileObject(new File("match")))
     val matchedFiles = FilterChecker("match") findMatchedFiles listOfFiles
-    assert(matchedFiles == List(matchingFile))
+    assert(matchedFiles == List(FileObject(new File("match"))))
   }
   
   "FilterChecker passed a list with a directory that matches the filter" should
   "should not return the directory" in {
-    val listOfIOObjects = List(new FileObject(new File("random")), new DirectoryObject(new File("match")))
+    val listOfIOObjects = List(FileObject(new File("random")), DirectoryObject(new File("match")))
     val matchedFiles = FilterChecker("match") findMatchedFiles listOfIOObjects
     assert(matchedFiles.isEmpty)
   }
