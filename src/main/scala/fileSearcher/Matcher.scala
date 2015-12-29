efa4ea0fbc25eb5ac5f8ccecfd2bcb93d21238b4
Daniel Parks <daniel.parks@bbc.co.uk>
2015-12-29 13:28:50 +0000

    added logic to matcher :: tests passing

--- src/main/scala/fileSearcher/Matcher.scala
+++ src/main/scala/fileSearcher/Matcher.scala
@@ -4,4 +4,14 @@ import java.io.File
 
 class Matcher(filter: String, rootLocation: String) {
   val rootIOObject = FileConverter.convertToIOObject(new File(rootLocation))
+
+  def execute() = {
+    val matchedFiles = rootIOObject match {
+      case file : FileObject if FilterChecker(filter) matches file.name => List(file)
+      case directory : DirectoryObject => ???
+      case _ => List()
+    }
+
+    matchedFiles map(iOObject => iOObject.name)
+  }
 }
